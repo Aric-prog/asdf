924b6d504c1217950003208717e84e0d0e5aac2e
Aric Hernando <hernandoaric@gmail.com>
2019-11-19 15:04:47 +0700

    -

---
+++ Exercise1_Location.py
@@ -0,0 +1,15 @@
+places = ['Vienna', 'Japan', 'Hell itself', 'UK', 'Monaco']
+
+print(places)
+
+print(sorted(places))
+print(places)
+print(sorted(places, reverse=True))
+places.reverse()
+print(places)
+places.reverse()
+print(places)
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
