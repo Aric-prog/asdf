74073f3a5a0a5233941acd1bd10dcdc91965aa9c
Aric Hernando <hernandoaric@gmail.com>
2019-11-20 11:49:23 +0700

    individual task no 1 and 2

---
+++ IndividualTaskNo2.py
@@ -0,0 +1,6 @@
+student1 = 80.0
+student2 = 90.0
+student3 = 66.5
+
+average = (student1 + student2 + student3) / (3 * 100)
+print(average * 100)
---
+++ IndividualTaskNo3.py
@@ -0,0 +1,21 @@
+class1 = 32
+class2 = 45
+class3 = 51
+
+group1 = class1 / 5
+group2 = class2 / 7
+group3 = class3 / 6
+
+leftover1 = class1 % 5
+leftover2 = class2 % 7
+leftover3 = class3 % 6
+
+print("Number of students in each group.")
+print("Class 1 : " + group1)
+print("Class 2 : " + group2)
+print("Class 3 : " + group3)
+
+print("Number of students leftover.")
+print("Class 1 : " + leftover1)
+print("Class 2 : " + leftover2)
+print("Class 3 : " + leftover3)
