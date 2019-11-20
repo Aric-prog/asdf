23082b46219d6893833a4b8b80c88295b93ef7fc
Aric Hernando <hernandoaric@gmail.com>
2019-11-20 11:58:16 +0700

    updated with function

--- IndividualTaskNo2.py
+++ IndividualTaskNo2.py
@@ -1,6 +1,10 @@
+def getAverage(student1,student2,student3):
+    average = (student1 + student2 + student3) / (3 * 100)
+    return average
+
 student1 = 80.0
 student2 = 90.0
 student3 = 66.5
 
-average = (student1 + student2 + student3) / (3 * 100)
+average = getAverage(student1,student2,student3)
 print(average * 100)
--- IndividualTaskNo3.py
+++ IndividualTaskNo3.py
@@ -1,21 +1,22 @@
+def outputText(num1,num2,num3):
+    print("Class 1 : " + str(num1))
+    print("Class 2 : " + str(num2))
+    print("Class 3 : " + str(num3))
+
 class1 = 32
 class2 = 45
 class3 = 51
 
-group1 = class1 / 5
-group2 = class2 / 7
-group3 = class3 / 6
+group1 = class1 // 5
+group2 = class2 // 7
+group3 = class3 // 6
 
 leftover1 = class1 % 5
 leftover2 = class2 % 7
 leftover3 = class3 % 6
 
 print("Number of students in each group.")
-print("Class 1 : " + group1)
-print("Class 2 : " + group2)
-print("Class 3 : " + group3)
+outputText(group1,group2,group3)
 
 print("Number of students leftover.")
-print("Class 1 : " + leftover1)
-print("Class 2 : " + leftover2)
-print("Class 3 : " + leftover3)
+outputText(leftover1,leftover2,leftover3)
